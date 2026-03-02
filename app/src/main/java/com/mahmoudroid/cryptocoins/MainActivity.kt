6558cbf5d4b3960501b832d9373dd32fd2cb9a30
Mahmoud Ali <maboelenen555@gmail.com>
2026-03-02 04:40:11 +0200

    implement nav logic

--- app/src/main/java/com/mahmoudroid/cryptocoins/MainActivity.kt
+++ app/src/main/java/com/mahmoudroid/cryptocoins/MainActivity.kt
@@ -4,26 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.mahmoudroid.cryptocoins.ui.theme.CryptoCoinsTheme
+import com.mahmoudroid.cryptocoins.views.Screen
+import com.mahmoudroid.cryptocoins.views.coindetails.CoinDetailsScreen
+import com.mahmoudroid.cryptocoins.views.coinlist.CoinListScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CryptoCoinsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+
+                        }
+                        composable(
+                            route = Screen.CoinDetailsScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailsScreen()
+
+                        }
+                    }
                 }
             }
         }
